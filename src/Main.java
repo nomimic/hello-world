935b49df51878497f505fc3ef99f4be981062286
USER1 <nhnent@AL01506651.local>
2017-01-24 14:10:05 +0900

    Revert "이상한짓 다시"
    This reverts commit 1b7deaf5d63dece58dff3bf6f73c4aa789da4565.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
